af98dd782ccf9d01e5e98dab5797bd640f8d6c29
Shuangji Lei <sjlei24@mail.ustc.edu.cn>
2026-06-14 16:34:30 +0800

    init: 个人主页

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'leisj24 · 个人主页';
+export const SITE_DESCRIPTION = '中国科学技术大学（USTC）本科生 leisj24 的个人主页与技术博客。';
